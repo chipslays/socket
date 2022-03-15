c06e619185513b5db4ca25b7f2bfde99af94abbc
chipslays <gorshkov34@gmail.com>
2022-03-15 15:08:08 +0400

    wip

--- src/Groups.php
+++ src/Groups.php
@@ -16,6 +16,7 @@ class Groups
 
     public function create(string $name, array $data = []): void
     {
+        $data['id'] = $name;
         $data['connections'] = $data['connections'] ?? [];
         $this->groups[$name] = $data;
     }
